1d53d7b27a9e5cb2cdcc1e45acb3e6eba45416dc
AlexandrM <jibrilfl@gmail.com>
2023-06-27 21:15:17 +0500

    add routing

--- Routing/app.js
+++ Routing/app.js
@@ -8,17 +8,41 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-Type', 'text/html');
 
-    if (req.url === '/') { // Определяем url запроса
-        fs.readFile('./views/index.html', (err, data) => { // Загружаем файл стартовой страницы
-            if (err) {
-                console.log(err);
-                res.end(); // Всегда возвращем контроль браузеру
-            } else {
-                res.write(data); // Записываем файл
-                res.end(); // Возвращаем контроль браузеру
-            }
-        })
+    const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+    let basePath = '';
+
+    switch (req.url) {
+        case '/':
+        case '/home':
+        case '/index.html':
+            basePath = createPath('index');
+            res.statusCode = 200;
+            break;
+        case '/about-us':
+            res.statusCode = 301;
+            res.setHeader('Location', '/contacts');
+            res.end();
+            break;
+        case '/contacts':
+            basePath = createPath('contacts');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('error');
+            res.statusCode = 404;
+            break;
     }
+
+    fs.readFile(basePath, (err, data) => { // Загружаем файл стартовой страницы
+        if (err) {
+            console.log(err);
+            res.statusCode = 500;
+            res.end(); // Всегда возвращем контроль браузеру
+        } else {
+            res.write(data); // Записываем файл
+            res.end(); // Возвращаем контроль браузеру
+        }
+    })
 
 });
 
